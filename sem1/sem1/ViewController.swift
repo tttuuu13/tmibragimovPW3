00eb4262cc37fa56a645650a9c097331ab4aeff0
tttuuu13 <tim.ib.222005@gmail.com>
2024-09-30 18:55:57 +0300

    sem1

---
+++ sem1/sem1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sem1
+//
+//  Created by тимур on 30.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
