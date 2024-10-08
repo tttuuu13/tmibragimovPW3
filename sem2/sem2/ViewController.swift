65608da2ff86ce2e3b02e01f01beddcfa8887684
tttuuu13 <tim.ib.222005@gmail.com>
2024-10-08 18:47:47 +0300

    sem2 added

---
+++ sem2/sem2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sem2
+//
+//  Created by тимур on 08.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
